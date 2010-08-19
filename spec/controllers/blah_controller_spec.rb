c0b22da7e4e181e78d9c75cf8025186f6e0c67a1
Bret Weinraub <bret@ckuru.com>
2010-08-19 17:31:48 +0000

    Don't use a method injected by the controller helper

--- spec/controllers/blah_controller_spec.rb
+++ spec/controllers/blah_controller_spec.rb
@@ -1,12 +1,11 @@
 require 'spec_helper'
-require RAILS_ROOT + "/vendor/plugins/meteor/init.rb"
 
 describe BlahController do
   
   before(:all) do
-    @content = render_meteor_widget(::Meteor::Widget::RemoteContent::Spec.new(:url           => "http://www.aura-software.com",
-                                                                              :remote_dom_id => ".section.content-body",
-                                                                              :partial       => "blah/test.rhtml"))
+    @content = ::Meteor::Widget::RemoteContent::Spec.new(:url           => "http://www.aura-software.com",
+                                                         :remote_dom_id => ".section.content-body",
+                                                         :partial       => "blah/test.rhtml").render(:controller => controller)
   end
   
   it "should render the specified remote content" do
